103c26b00296fe431c83fd185b5e582786723738
Ryan LaBouve <imryanlabouve@gmail.com>
2016-04-09 11:43:39 -0500

    Spike post model

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+Toyblog::Application.config.secret_token = 'ef9bd0a345a6d9a14d060638db67124b9c09612c88665907888269610cb9aaa47e9cf3714add1db627af75ace604207ef027a3c04148d22ad3c4e23385829f9a'
